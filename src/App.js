82cb0f8a65fea293ab3798245c3759459a926f8f
Sorrel Bri <sorrel.bri.june@gmail.com>
2019-11-25 20:35:46 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" data-testid="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Phono Change Applier
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
+import { exportAllDeclaration } from '@babel/types';
+import {render} from '@testing-library/react';
+import extendExpect from '@testing-library/jest-dom/extend-expect'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('App', () => {
+  it('renders the correct title', () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId('App')).toHaveTextContent('Phono Change Applier');
+  })
+})
