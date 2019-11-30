21a5a62b60d19f3cfea905f2f3e12bd3ffd360cf
Sorrel Bri <sorrel.bri.june@gmail.com>
2019-11-29 16:51:34 -0800

    add function for increasing epochs

--- src/components/Epochs.js
+++ src/components/Epochs.js
@@ -3,11 +3,21 @@ import './Epochs.scss';
 
 import SoundChangeSuite from './SoundChangeSuite';
 
+const addEpoch = (e, props) => {
+  console.log(props)
+  e.preventDefault()
+  props.setEpochs([...props.epochs, {name: '', changes:['']}])
+  console.log(props)
+}
+
 const Epochs = props => {
   return (
     <div className="Epochs" data-testid="Epochs">
       <h3>Sound Change Epochs</h3>
-      {props.epochs ? props.epochs.map(epoch => <SoundChangeSuite key={epoch.name} epoch={epoch}/>) : <></>}
+      {props.epochs ? props.epochs.map((epoch, idx) => <SoundChangeSuite key={`epochname-${idx}`} epoch={epoch}/>) : <></>}
+      <form onSubmit={e=>addEpoch(e, props)}>
+        <input type="submit" name="add-epoch" value="Add Epoch" ></input>
+      </form>
     </div>
   );
 }
